a48c582e707b1d6a43d2c6c78af64478016249d3
Uniplac <github@uniplaclages.edu.br>
2022-08-16 11:25:39 -0300

    começo

---
+++ pages/components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer
+      className="bg-white text-center text-base fixed left-0 bottom-0 flex justify-center items-center w-full"
+    >
+      <div className="text-gray-700 p-3">
+        © Copyright:
+        <a href="https://tailwind-elements.com/">{" </NIU>"}</a>
+      </div>
+    </footer>
+  );
+}
